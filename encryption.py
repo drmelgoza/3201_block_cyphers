33f99198fc8abc33ce30d71ce44ad590dfbac912
Diego Melgoza <drmelgoza@icloud.com>
2026-09-13 16:08:22 -0700

    annotated ecb function

--- encryption.py
+++ encryption.py
@@ -9,35 +9,38 @@ def get_new_key(size=128):
     return byte_key
 
 #PART 1:
-#TODO: ECB cypher
 def encrypt_ecb(key, file="mustang.bmp"):
     cipher = AES.new(key, AES.MODE_ECB)
+    #Open file using "br" mode to read bytes.
     with open(file, "br") as f:
-        #get file size (in bytes) using seek method
+        #get file size (in bytes w/o header) using seek method.
+        #Go to last byte in file, and subtract 54 bytes, then return to the beginning.
         f.seek(0, 2)
         file_size = f.tell() - 54
         f.seek(0)
-        #read file header and store for later
+        #read file header and start the bytestream for the encrypted file.
         new_bmp = f.read(54)
         blocks_read = 0
-        #all in bytes, if not one whole block left, do padding
-        while file_size - blocks_read * 16 > 16:
+        #While there are 16 byte blocks remaining, read and encrypt the file.
+        #Encrypted data stored in the new_bmp variable
+        while file_size - blocks_read * 16 >= 16:
             blocks_read += 1
             data = f.read(16)
             encrypted_data = cipher.encrypt(data)
             new_bmp += encrypted_data
 
-        if (file_size - blocks_read * 16) == 0:
-            # do something with the file and make new file
-            pass
-        else:
-            # execute padding to make file perfect.
+        #if file has data remaining, add padding
+        if (file_size - blocks_read * 16) != 0:
+            #read remaining data
             padded_data = f.read(16)
+            #get remaining bytes, and build the padding bytes
             bytes_remaining = file_size - blocks_read * 16
-            pad_bytes = 16 - (bytes_remaining % 16)
-            pad_byte = pad_bytes.to_bytes(1, byteorder='big')
-            padding = pad_byte * pad_bytes
+            pad_bytes_needed = 16 - (bytes_remaining % 16)
+            pad_byte = pad_bytes_needed.to_bytes(1, byteorder='big')
+            padding = pad_byte * pad_bytes_needed
+            #add padding to remaining data
             padded_data += padding
+            #encrypt and add to file
             encrypted_data = cipher.encrypt(padded_data)
             new_bmp += encrypted_data
 
